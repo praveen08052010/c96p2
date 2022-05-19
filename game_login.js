93b4427b76fc47538149abada9fd61a56a379aca
praveen08052010 <97897309+praveen08052010@users.noreply.github.com>
2022-05-19 19:59:23 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,15 @@
+// Create function addUser()
+function addUser() ____ {
+  // Get value of user by id player1_name_input and player2_name_input
+  player1_name_input = value,player2_name_input = value;
+  // Store these values locally
+  localStorage.getItem(player1_name_input,player2_name_input)
+  //Assign "game_page.html" to window.location
+  window.location("game_page.html")
+}
+funtion RTCRtpSender()
+[
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2)
+]
